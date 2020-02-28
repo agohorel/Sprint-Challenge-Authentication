a1b50c41be39c509828a4ec292442932a66c086f
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-28 11:30:47 -0500

    add auth routes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
-const router = require('express').Router();
+require("dotenv").config();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("./auth-model.js");
+
+router.post("/register", async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    const hashed = bcrypt.hashSync(password, 12);
+    password = hashed;
+    const newUser = await db.insert({ username, password });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await db.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ msg: `welcome, ${user.username}!` });
+    } else {
+      res.status(401).json({ msg: "invalid credentials" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
 });
 
 module.exports = router;
